5fa16b18ea0f6d0e1c6ffc74c7248591b7e6a838
Ian <0306pitt@gmail.com>
2022-12-15 10:08:27 -0500

    component adjustment

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import Home from "./pages/Home";
 import Workout from "./pages/Workout";
+import Login from "./pages/Login"
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
@@ -11,30 +12,19 @@ function App() {
   return (
     <Router>
       <div className="App">
-
         <Header />
         <div className="content">
-          <Switch>
-            <Route 
-            path="/" 
-            elements={<Home />}
-            >
-              <Home />
-            </Route>
-            <Route 
-            path="/workouts" 
-            elements={<Workout />}
-            >
-              <Workout />
-            </Route>
-          </Switch>
-          <p>Fill in homepage info and content here</p>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/workouts" element={<Workout/>} />
+            <Route path="/login" element={<Login/>} />
+          </Routes>
         </div>
-        {/* <Footer /> */}
-
+        <Footer />
       </div>
     </Router>
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Nav, NavLink, NavMenu } from "./NavbarElements"
 
 const Header = () => {
-  const navbarHeader = "Navbar";
   return (
     <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
       <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
         <h1>Git-Fit</h1>
-        <Link className="text-dark" to="/home">
-          <h2 className="m-0" style={{ fontSize: "20px" }}>
-            Home
-          </h2>
-        </Link>
-        <Link className="text-dark" to="/workouts">
-          <h2 className="m-0" style={{ fontSize: "20px" }}>
-            Workouts
-          </h2>
-        </Link>
+        <Nav>
+          <NavMenu>
+            <NavLink to="/Home" activeStyle>
+              Home
+            </NavLink>
+            <NavLink to="/Workouts" activeStyle>
+              Tracker
+            </NavLink>
+          </NavMenu>
+        </Nav>
       </div>
     </header>
   );
